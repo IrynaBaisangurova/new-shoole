cd5c01bd43a0a9994bcd022a0bef408bc4e79372
Iryna <laudaeren@ukr.net>
2025-03-05 00:27:51 +0200

    add all

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './js/hero.js';
+import './js/counter.js';
+import './js/mod.js';
+import './js/about-me.js';
+import './js/reviews.js';
+import './js/swiper.js';
+import './js/app.js';
+import './checkout.js';
+
+
+
+
+
+
+
