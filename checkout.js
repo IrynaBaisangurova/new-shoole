eec6a36345a7bef12ccd6fa5e407f944966b1156
IrynaBaisangurova <92602692+IrynaBaisangurova@users.noreply.github.com>
2025-03-06 18:42:03 +0000

    Deploying to gh-pages from @ IrynaBaisangurova/new-shoole@add315aed096d607f540f9eaa1ae7c257112ea45 🚀

--- checkout.js
+++ checkout.js
@@ -1,4 +1,4 @@
-import"./assets/styles-JhtLTKVy.js";let r=[];function c(){var i=document.cookie.split("; ").find(e=>e.startsWith("listCart="));i&&(r=JSON.parse(i.split("=")[1]))}c();o();function o(){let i=document.querySelector(".returnCart .list");i.innerHTML="";let e=document.querySelector(".totalQuantity"),s=document.querySelector(".totalPrice"),a=0,n=0;r&&r.forEach(t=>{if(t){let l=document.createElement("div");l.classList.add("item"),l.innerHTML=`<img src="${t.image}">
+import"./assets/styles-D3Zv_nFo.js";let r=[];function c(){var i=document.cookie.split("; ").find(e=>e.startsWith("listCart="));i&&(r=JSON.parse(i.split("=")[1]))}c();o();function o(){let i=document.querySelector(".returnCart .list");i.innerHTML="";let e=document.querySelector(".totalQuantity"),s=document.querySelector(".totalPrice"),a=0,n=0;r&&r.forEach(t=>{if(t){let l=document.createElement("div");l.classList.add("item"),l.innerHTML=`<img src="${t.image}">
                     <div class="info">
                         <div class="name">${t.name}</div>
                         <div class="price">₴${t.price} / 1 ${t.unit}</div>
